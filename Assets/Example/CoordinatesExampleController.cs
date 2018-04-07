d64c8dcf8a6c3b6778150ebddeebd58752b8e1bb
Hubris / Mac <yes@hubris.xyz>
2018-04-07 23:19:07 +0100

    first!

---
+++ Assets/Example/CoordinatesExampleController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Hubris;
+
+public class CoordinatesExampleController : MonoBehaviour
+{
+    [Header("Polar / Cylindrical")]
+    public Polar.BasePlane basePlane;
+    [Range(-10f, 10f)]
+    public float pRadius;
+    [Range(0, Arithmos.TWO_PI)]
+    public float pAzimuth;
+    [Range(-5f, 5f)]
+    public float pHeight = 0f;
+
+    private Polar polarCoord;
+    private Transform polarCylindrical;
+
+    [Header("Spherical")]
+    [Range(-10f, 10f)]
+    public float sRadius;
+    [Range(0, Arithmos.TWO_PI)]
+    public float sAzimuth;
+    [Range(-Arithmos.HALF_PI, Arithmos.HALF_PI)]
+    public float sPolar = 0f;
+
+    public Spherical sphericalCoord;
+    private Transform spherical;
+
+    void Start ()
+    {
+        polarCoord = new Polar();
+        polarCylindrical = gameObject.GetComponentsInChildren<Transform>()[0];
+
+        sphericalCoord = new Spherical();
+        spherical = gameObject.GetComponentsInChildren<Transform>()[1];
+    }
+
+    void Update ()
+    {
+        polarCoord.Set(pRadius, pAzimuth, pHeight, basePlane);
+        polarCylindrical.position = polarCoord;
+
+        sphericalCoord.Set(sRadius, sAzimuth, sPolar);
+        spherical.position = sphericalCoord;
+    }
+}
---
+++ Assets/Example/CoordinatesExampleFun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Hubris;
+
+public class CoordinatesExampleFun : MonoBehaviour
+{
+    const int num = 100;
+
+    [Range(-1f, 1f)]
+    public float qX, qY, qZ, qW;
+
+    Transform capsParent;
+    Transform spheParent;
+
+    void Start ()
+	{
+        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        capsParent = gameObject.GetComponentsInChildren<Transform>()[2];
+
+
+        for (int i = 0; i < num; i++)
+        {
+            float normalize = (float)i / num;
+            Polar pos = new Polar(10f + Arithmos.SinNormalized(normalize * Arithmos.TWO_PI * 12f) * 5f, normalize * Arithmos.TWO_PI, 0f, Polar.BasePlane.XY);
+
+            normalize *= 2f;
+            normalize -= Mathf.Floor(normalize);
+            normalize *= 2f;
+
+            Quaternion rot = new Quaternion(0f, 0f, normalize - 1f, 1f);
+
+            Instantiate(obj, pos, rot, capsParent);
+        }
+
+        Destroy(obj);
+        obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        spheParent = gameObject.GetComponentsInChildren<Transform>()[1];
+
+        for (int i = 0; i < num; i++)
+        {
+            float normalize = (float)i / num;
+            Spherical pos = new Spherical(25f, normalize * Arithmos.TWO_PI, (normalize - 0.5f) * Arithmos.PI);
+            Quaternion rot = new Quaternion();
+            Instantiate(obj, pos, rot, spheParent);
+        }
+
+        Destroy(obj);
+
+    }
+	
+	void Update ()
+	{
+        capsParent.Rotate(0f, 0f, Mathf.Sin(Time.time));
+        spheParent.Rotate(0f, Mathf.Sin(Time.time) * Arithmos.PI, 0f);
+	}
+}
